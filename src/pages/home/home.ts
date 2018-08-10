3d90e5774ee517ee6b422f0fa06be3ca6fa3e18a
names <wprecia4@gmail.com>
2018-08-10 16:04:58 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+name1='';
+name2='';
 
-  constructor(public navCtrl: NavController) {
+constructor(private alertCtrl: AlertController) {
 
-  }
+}
 
+presentAlert() {
+  let alert = this.alertCtrl.create({
+    title: 'love',
+    subTitle:'%',
+    buttons: ['Dismiss']
+  });
+  alert.present();
+}
+get score(){
+const letters = (this.name1 + this.name2);
+let sum =0;
+for (let i=0; i<letters.length; i++){sum +=letters.charCodeAt(i);
+}
+return sum % 101;
+}
 }
